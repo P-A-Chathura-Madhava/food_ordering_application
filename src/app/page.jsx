5059ff757cc47451ffe3f5975c761b33bfc6ae0f
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-02-16 20:43:27 +0530

    Apply Menu UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
 import Navbar from "@/components/layout/Navbar";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
   <>
     <Navbar />
     <Hero />
+    <HomeMenu />
   </>
   );
 }
