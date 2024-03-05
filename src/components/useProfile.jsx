a47a4668b8b66f44f4b3d17641dbfcc2303dae59
P A Chathura Madhava <chathuratechdev@gmail.com>
2024-03-05 10:24:10 +0530

    Apply Categories functions

--- src/components/useProfile.jsx
+++ src/components/useProfile.jsx
@@ -1,20 +1,17 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 
-function useProfile() {
-    const [data, setData] = useState(false);
-    const [loading, setLoading] = useState(true);
-    useEffect(()=>{
-      setLoading(true);
-      fetch("/api/profile").then(response => {
-        response.json().then(data => {
-          setData(data);
-          setLoading(false);
-        });
-      })
-    }, []);
+export function useProfile() {
+  const [data, setData] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    fetch('/api/profile').then(response => {
+      response.json().then(data => {
+        setData(data);
+        setLoading(false);
+      });
+    })
+  }, []);
 
   return {loading, data};
 }
-
-export default useProfile
